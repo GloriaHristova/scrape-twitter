956858f04bcec467647f245ca0e7f85b97143a82
Gloria Hristova <41238594+GloriaHristova@users.noreply.github.com>
2018-07-23 16:00:28 +0300

    Adding scripts

---
+++ scrape-tweets.py
@@ -0,0 +1,58 @@
+# Load necessary modules and packages
+import tweepy
+import authenticate
+import scrapeoptions
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+
+# Authenticate with Twitter using tweepy (must create a Twitter Application - check read.me)
+auth = tweepy.OAuthHandler(authenticate.api_key, authenticate.api_Secret)
+auth.set_access_token(authenticate.access_token, authenticate.access_token_secret)
+api = tweepy.API(auth)
+
+# Rewrite the StreamListener() class (for more details - check tweepy documentation and repo - https://github.com/tweepy/tweepy)
+# Make use of on_data() method of StreamListener() class in order to obtain raw data from Twitter Streaming API 
+# More about the Tweet() object - https://developer.twitter.com/en/docs/tweets/data-dictionary/overview/tweet-object
+
+class MyListener(StreamListener):
+    
+    def __init__(self,limit=10):
+        
+        super().__init__()
+        self.counter = 0
+        self.limit = limit
+     
+    def on_data(self, data):
+
+        # ! Retweets and quoted tweets are EXCLUDED from the sample
+        if 'retweeted_status' in data: 
+            return
+
+        if 'quoted_status' in data: 
+            return
+        
+        self.counter += 1
+        
+        # Append each new tweet to json
+        try:
+            with open(scrapeoptions.nameoutputfile, 'a') as f:
+                f.write(data) 
+        except:
+            print("Error: on_data.")
+             
+        # Disconnect if set limit is exceeded
+        if self.counter < self.limit:
+            return True
+        else:
+            return False
+ 
+    def on_error(self, status):
+        print(status)
+        # Disconnect if we are rate limited from Twitter
+        if status == 420: 
+            return False
+
+# Start the listener   
+stream_listener =  MyListener(limit = scrapeoptions.setlimit) # the default limit is 10 tweets
+twitter_stream = Stream(auth,stream_listener)
+twitter_stream.filter(track=scrapeoptions.keywords, languages = scrapeoptions.tweetlang)
---
+++ scrapeoptions.py
@@ -0,0 +1,12 @@
+# Set stream parameters
+nameoutputfile = 'exampleoutput.json'
+setlimit = 10
+keywords = ['data science']
+tweetlang = ['en']  
+
+# Important Notes:
+# About keywords: https://developer.twitter.com/en/docs/tweets/rules-and-filtering/overview/standard-operators.html
+# About stream parameters: https://developer.twitter.com/en/docs/tweets/filter-realtime/guides/basic-stream-parameters
+# Exact matching of phrases is NOT supported (only partial)!
+# Keyword tracking should be case-insensitive BUT double check the output!
+# Supported languages (some are available only with the Premium and Enterprise tier products.): https://developer.twitter.com/en/docs/tweets/rules-and-filtering/overview/premium-operators.html
